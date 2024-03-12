d7c3932b259428f01cea6e534985a29a0649572c
Wendyamanya <wendixod@gmail.com>
2024-03-12 18:12:17 +0300

    first commit

---
+++ variables.dart
@@ -0,0 +1,16 @@
+void main ( ) {
+  //variables in dart language
+  String school ='PLP AFRICA ACADEMY';
+  int total = 400;
+  double GPA = 4.0;
+  num year = 2024;
+  bool ismarried =true;
+  // lets print our variables
+  print(school);
+  print(school);
+  print(total);
+  print(GPA);
+  print(year);
+  print(ismarried);
+
+}
